fc1dcf73d54ff1a154b4781b3cca72b69eaea10d
A11002500 <116183271+A11002500@users.noreply.github.com>
2023-03-28 03:58:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random()*6 )+1;
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomImageSrc = "images/"+randomDiceImage;
+// change image1
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSrc);
+//end
+
+var randomNumber2 = Math.floor(Math.random()*6 )+1;
+var randomDiceImage2 = "dice"+randomNumber2+".png";
+var randomImageSrc2 = "images/"+randomDiceImage2;
+
+//change image2
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSrc2);
+
+//text above
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = ":) Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins :)";
+}
+else{
+    document.querySelector("h1").innerHTML = ":| Draw :|";
+}
